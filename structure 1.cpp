2b5a6aa1fe6c1b77376028896c4f6c1209c00e46
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-10 20:04:20 +0530

    Add files via upload

---
+++ structure 1.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+struct emp
+{
+int eno;
+char ename[20];
+float sal;
+}e1[100];
+int main()
+{
+	void disp(struct emp e1[100],int n);
+	int i,n;
+	printf("enter limit");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{		
+	   printf("enter eno ename sal");
+	   scanf("%d%s%f",&e1[i].eno,&e1[i].ename,&e1[i].sal);	
+	}
+	disp(e1,n);
+}
+	void disp(struct emp e1[100],int n)
+{
+
+	int i,max,index=0;
+	max=e1[0].sal;
+	for(i=0;i<n;i++)
+	 {
+	   if(e1[i].sal>max)
+	   {
+	    max=e1[i].sal;
+	    index=i;
+	  }
+}
+	printf("%d\t%s\t%f",e1[index].eno,e1[index].ename,e1[index].sal);
+}
