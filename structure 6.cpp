bc28bef941126b59c0c89df0d59e1f16e1b38921
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-10-15 17:57:07 +0530

    Add files via upload

---
+++ structure 6.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+  struct student
+  {
+    int rno;
+    char name[20];
+    float per;
+   }s1[100];
+   int i,n,num,flag=0;
+   printf("enter limit");
+   scanf("%d",&n);
+   printf("enter student info");
+   for(i=0;i<n;i++)
+	{
+	   printf("enter roll number ,name and percentage");
+	  scanf("%d%s%f",& s1[i].rno,& s1[i].name,& s1[i].per);
+	}
+   printf("enter roll number to search");
+   scanf("%d",&num);
+   for(i=0;i<n;i++)
+	{
+	if(s1[i].rno==num)
+	 { 
+	  flag=1;
+	   break;
+	 }
+	}
+ if(flag==1)
+{
+   printf("record is found");
+   printf("student percentage,name=%s\n%f",s1[i].name,s1[i].per);
+}
+else
+printf("record not found");
+}
